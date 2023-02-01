d7d5639bc410a292e5ee077b15af102f7b79746d
no2ehi <graphist77@gmail.com>
2023-02-01 20:32:14 +0330

    add color palette component task

--- src/components/TasksSection/NewTask.jsx
+++ src/components/TasksSection/NewTask.jsx
@@ -20,12 +20,33 @@ const NewTask = () => {
     const maxDate = year + 1 + "-" + month.toString().padStart(2, "0") + "-" + day.toString().padStart(2, "0");
 
     const [showAddTask, setShowAddTask] = useState(false);
+
+    const initialColorPalette = [
+        {
+            name: 'red',
+            code: '#F55050'
+        },
+        {
+            name: 'pantone',
+            code: '#F48484'
+        },
+        {
+            name: 'yellow',
+            code: '#F2CD5C'
+        },
+        {
+            name: 'aqua',
+            code: '#46C2CB'
+        },
+    ]
+    const [showColorPalette, setShowColorPalette] = useState(false);
+
     const [newTask, setNewTask] = useState({
             title: "",
-            important: false,
             description: "",
             date: todayDate,
             label: "",
+            important: false,
             completed: false,
             id: '',
         });
@@ -41,22 +62,25 @@ const NewTask = () => {
 
     const changeTaskHandler = (e) => {
         const name = e.target.name;
-        const value = e.target.value === 'checkbox' ? e.target.checked  : e.target.value;
+        const value = e.target.value === 'checkbox' ? !e.target.checked  : e.target.value;
 
         setNewTask({
             ...newTask,
             [name]: value
         })
+
+        
     }
 
     const addNewTaskHandler = (e) => {
         e.preventDefault();
-        if(newTask.title && newTask.description ) {
+        if(newTask.title && newTask.description && newTask.label) {
             const taskSubmit = {
                 ...newTask,
                  id: Date.now().toString()
             }       
             dispatch(tasksActions.addNewTask(taskSubmit));
+            setShowAddTask(false);
         }
         setNewTask({
             title: "",
@@ -67,7 +91,7 @@ const NewTask = () => {
             completed: false,
             id: '',
         });
-        setShowAddTask(false);  
+          
     }
 
     return(
@@ -106,47 +130,94 @@ const NewTask = () => {
                         type="text"
                         name="description"
                         placeholder="Take a Task..."
-                        className={`${showAddTask ? 'h-auto' : 'h-6'} bg-slate-400 placeholder:text-slate-800 font-medium outline-none w-full resize-none`}  
+                        className={`${showAddTask ? 'h-24' : 'h-6'} bg-slate-400 placeholder:text-slate-800 font-medium outline-none w-full resize-none`}  
                         value={newTask.description}
                         onChange={changeTaskHandler}
                     />
 
+                    {!showAddTask && (
+                        <div className="flex">
+                            <button className={classBtnIcon}>
+                            <BellIcon  />
+                            </button>
+                            <button className={classBtnIcon}>
+                                <StarLine  />
+                            </button>
+                            <button className={classBtnIcon}>
+                                <CheckIcon  />
+                            </button>
+                        </div>
+                    )}
+
                     {/* Labels */
                     showAddTask && (
-                        <div className="flex justify-between">
-                            <label>
-                                Select a Label:
-                            <select
-                                type="select"
-                                name="label"
-                                value={newTask.label}
-                                onChange={changeTaskHandler}
-                                className="w-full  rounded-lg bg-slate-200 p-2 border-1"
-                            >
-                                {labels.map((label) => (
-                                    <option 
-                                        value={label}
-                                        key={label}
-                                        >
-                                            {label}
-                                    </option>
-                                ))}
-                            </select>
-                            </label>
+                        <div className="flex justify-between gap-4">
 
-                            <label>
-                            Date
-                            <input 
-                                type="date"
-                                name="date"
-                                value={newTask.date}
-                                min={todayDate}
-                                max={maxDate}
-                                onChange={changeTaskHandler}
-                                className="w-full rounded-lg bg-slate-200 p-2 border-1"
-                                required
-                            />
-                            </label>
+                            <div
+                                onClick={() => setShowColorPalette((prev) => !prev)}
+                                className="flex w-1/3 jusitfy-between items-center bg-slate-200 rounded-lg px-2 font-medium transition hover:bg-slate-500">
+                                <StarLine  />
+                                <span>Select a Color</span>
+                                {
+                                    showColorPalette && (
+                                        <div className="absolute w-48 h-22 bg-slate-100 rounded-md p-2 z-20 shadow ">
+                                            <ul className="flex flex-col">
+                                                {initialColorPalette.map((color) => (
+                                                    <div key={color.code} className={`bg-[${color.code}] m-1 pl-6 w-6 h-6 rounded-full `}>{color.code}</div>
+                                                ))}
+                                            </ul>
+                                        </div>
+                                    )
+                                }
+                            </div>
+
+                            <div className="flex w-1/3 items-center justify-between bg-slate-200 rounded-lg px-2 font-medium transition hover:bg-slate-500">
+                                <StarLine  />
+                                <select
+                                    type="select"
+                                    name="label"
+                                    value={newTask.label}
+                                    onChange={changeTaskHandler}
+                                    className="bg-transparent outline-none rounded-lg bg-slate-200 p-2"
+                                    required
+                                >
+                                    <option value="" disabled>Labels</option>
+                                    {labels.map((label) => (
+                                        <option 
+                                            value={label}
+                                            key={label}
+                                            >
+                                                {label}
+                                        </option>
+                                    ))}
+                                </select>
+                            </div>
+
+                            {/* <div className="flex items-center bg-slate-200 rounded-lg px-2 font-medium transition hover:bg-slate-500">
+                                <StarLine  />
+                                <input 
+                                    type="file"
+                                    name="file"
+                                    className="w-32 bg-transparent outline-none rounded-lg bg-slate-200 p-2"  
+                                />
+                            </div> */}
+
+                            <div className="flex w-1/3 items-center justify-between bg-slate-200 rounded-lg px-2 font-medium transition hover:bg-slate-500">
+                                <StarLine  />
+                                <input 
+                                    type="date"
+                                    name="date"
+                                    value={newTask.date}
+                                    min={todayDate}
+                                    max={maxDate}
+                                    onChange={changeTaskHandler}
+                                    className="bg-transparent outline-none rounded-lg bg-slate-200 p-2"
+                                    required
+                                />
+                            </div>
+
+
+                           
 
                         </div>
                     )}
@@ -158,7 +229,7 @@ const NewTask = () => {
                         'flex flex-row '} `} >
 
                         <div className="flex">
-                            <button className={classBtnIcon}>
+                            {/* <button className={classBtnIcon}>
                                 <StarLine  />
                             </button>
                             <button className={classBtnIcon}>
@@ -166,22 +237,39 @@ const NewTask = () => {
                             </button>
                             <button className={classBtnIcon}>
                                 <DateIcon  />
-                            </button>
+                            </button> */}
                             {showAddTask && (
                                 <>
-                                    <button className={classBtnIcon}>
-                                        <EditIcon  />
-                                    </button>
-                                    <button className={classBtnIcon}>
-                                        <CheckIcon  />
-                                    </button>
-                                    <button className={classBtnIcon}>
-                                        <DateIcon  />
-                                    </button>
+                                    <div className="flex items-center">
+                                        <label className="relative inline-flex items-center cursor-pointer">
+                                        <input 
+                                            type="checkbox" 
+                                            name="important"
+                                            value={newTask.important} 
+                                            onChange={changeTaskHandler}
+                                            className="sr-only peer" />
+                                        <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                                        <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Important</span>
+                                        </label>
+                                    </div>
+
+                                    <div className="flex items-center ml-4">
+                                        <label className="relative inline-flex items-center cursor-pointer">
+                                        <input 
+                                            type="checkbox"
+                                            name="completed"
+                                            value={newTask.completed} 
+                                            onChange={changeTaskHandler}
+                                            className="sr-only peer" />
+                                        <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                                        <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Completed</span>
+                                        </label>
+                                    </div>
+                                    
                                 </>
                             )}
                         </div>
-                        <button onClick={() => setShowAddTask(false)}
+                        <button
                             className={`${showAddTask ? 'visible bg-slate-300 py-1 px-3 rounded-md' : 'hidden'}`}>
                             Close
                         </button>
